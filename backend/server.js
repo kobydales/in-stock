6b6c848b8c602bdf94d81ffd33dcd9629839dda0
Koby Dales <kobydales@gmail.com>
2026-08-14 09:22:06 +0000

    Add API routes and set up nodemon for auto-restart

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,20 @@ app.get('/', (req, res) => {
   res.send('In-Stock API is running')
 })
 
+app.get('/api', (req, res) => {
+  res.json({ message: 'API is working' })
+})
+
+app.get('/api/test', (req, res) => {
+  res.json({
+    status: 'success',
+    data: [
+      { id: 1, name: 'Sample Item A' },
+      { id: 2, name: 'Sample Item B' }
+    ]
+  })
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
 })
